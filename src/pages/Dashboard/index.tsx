9ed14cd8e23a0e84dbcdd9734970233bb919b938
valber <valbermarcelino99@gmail.com>
2021-01-13 14:13:08 -0300

    Criando Rotas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// function Dashboard() {} é mais verboso pra falar o tipo
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
+
+// Switch faz o acesso a rota ser exclusivo, sõ da pra acessar uma ao mesmo tempo
