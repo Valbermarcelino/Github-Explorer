23007e0e0ab69496b8df1badc70a091a0e9bf15a
valber <valbermarcelino99@gmail.com>
2021-01-13 23:33:42 -0300

    conectando a API-2

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -31,6 +31,8 @@ const Dashboard: React.FC = () => {
     const repository = response.data;
 
     setRepositories([...repositories, repository]);
+
+    setNewRepo(''); // limpando barra de pesquisa
     // console.log(response.data);
     // console.log(newRepo);
     // Adição de um novo repositório
@@ -53,18 +55,20 @@ const Dashboard: React.FC = () => {
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://instagram.fubt1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/75280196_747899542397651_4926991176923348992_n.jpg?_nc_ht=instagram.fubt1-1.fna.fbcdn.net&_nc_ohc=zThdMKZGv8IAX9xeurU&tp=1&oh=87ddd62a2db124106da9cb3409b490b1&oe=602859FA"
-            alt="Valber"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scaleable React JS</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
